5129e4dab521be968aaf1da945b1f29cd6fc83f6
respectwo2 <respectwo@gmail.com>
2024-03-18 15:46:43 +0900

    rename: entity 이름 변경에 따른 import 추가

--- src/main/java/com/pswchat/chatservice/repository/ChatRepository.java
+++ src/main/java/com/pswchat/chatservice/repository/ChatRepository.java
@@ -2,8 +2,8 @@ package com.pswchat.chatservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.pswchat.chatservice.domain.ChatCollections;
+import com.pswchat.chatservice.domain.Chat;
 
-public interface ChatRepository extends MongoRepository<ChatCollections, String> {
+public interface ChatRepository extends MongoRepository<Chat, String> {
 
 }
