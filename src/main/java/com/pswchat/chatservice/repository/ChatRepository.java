1b29e1c5a86e315e432cef451093e90613617aa7
respectwo2 <respectwo@gmail.com>
2024-04-03 17:54:08 +0900

    style: component 코드 추가

--- src/main/java/com/pswchat/chatservice/repository/ChatRepository.java
+++ src/main/java/com/pswchat/chatservice/repository/ChatRepository.java
@@ -1,9 +1,11 @@
 package com.pswchat.chatservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pswchat.chatservice.domain.Chat;
 
+@Repository
 public interface ChatRepository extends MongoRepository<Chat, String> {
 
 }
