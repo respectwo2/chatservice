459e3524f134232ee4ac23ff9e25a6aacc320ba2
respectwo2 <respectwo@gmail.com>
2024-04-11 16:03:24 +0900

    refactor : 경로 관련 코드 클라이언트 측으로 변경

--- src/main/java/com/pswchat/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/pswchat/chatservice/config/WebSocketConfig.java
@@ -21,19 +21,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    	//핸들러 설정, 와일드카드(*) 옵션으로 모든 도메인을 허용시키고
-    	//withSockJS는 웹소켓을 지원하지 않는 브라우저에서도 사용가능하게만듬
-
-    	//SocketJS 오류 발생해 주석 처리 후 해당 코드 삭제
-    	//registry.addHandler(myWebSocketHandler, "/chat").setAllowedOrigins("*").withSockJS();
-    	registry.addHandler(myWebSocketHandler, "/chatRoom{room_id}")
-    			.setAllowedOrigins("*")
-   
-    			//세션끼리 연결해주는 메서드
-    			.addInterceptors(new HttpHandshakeInterceptor());
-
+        registry.addHandler(myWebSocketHandler, "/chatRoom/sockjs")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
-    
-    
-    
+
 }
--- src/main/java/com/pswchat/chatservice/controller/ViewController.java
+++ src/main/java/com/pswchat/chatservice/controller/ViewController.java
@@ -1,17 +1,9 @@
 package com.pswchat.chatservice.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.pswchat.chatservice.domain.Chat;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ViewController {
@@ -31,20 +23,4 @@ public class ViewController {
 	}
 
     
-	@PostMapping("/enterChatRoom")
-	public String enterChatRoom(@RequestParam("room_id") String room_id, @RequestParam("createdName") String createdName, HttpSession session) {
-	    session.setAttribute("room_id", room_id);
-	    session.setAttribute("createdName", createdName);
-
-	    return "redirect:/chatRoom" + room_id; 
-	}
-
-    
-    @GetMapping("/chatRoom/{room_id}")
-    public String chatRoom(@PathVariable("room_id") String room_id, Model model,HttpSession session) {
-    	model.addAttribute("room_id", room_id);
-        return "chatRoom"; 
-    }
-
-
 }
